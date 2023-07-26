0c8b0c5f1f8c3c33bfcb074d29a12540820b5485
unknown <toshconsultinc@gmail.com>
2023-07-26 15:37:27 +0100

    styling the input

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -1,48 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 const Convert = () => {
-    const [dollar, setDollar] = useState("")
-    const [naira, setNaira] = useState("")
-
+  const [dollar, setDollar] = useState("");
+  const [naira, setNaira] = useState("");
 
   const apiURL = 'https://dashboard.encryptbox.co.uk/api/v1/live/getbuyrate';
   const token = '0Coc24mjYhls8bPSZWYKGjVKHeWBhDjgqlqiFK4Hf9FsLN5HTMpRxeJ85pMwGx';
-  const handleChange = () =>{
+
+  const handleChange = (e) => {
+    const newDollarAmount = e.target.value;
+    setDollar(newDollarAmount);
 
     fetch(apiURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'token': token,
-    },
-    body: JSON.stringify({
-      "amount": dollar, 
-      "coin-name": "busd",
-    }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      const { coin } = data;
-      setNaira(coin);
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': token,
+      },
+      body: JSON.stringify({
+        "amount": newDollarAmount,
+        "coin-name": "busd",
+      }),
     })
-    .catch(error => {
-      console.error("Error when calculating", error);
-    });
-  }
-  
+      .then(response => response.json())
+      .then(data => {
+        const { coin } = data;
+        setNaira(coin);
+      })
+      .catch(error => {
+        console.error("Error when calculating", error);
+      });
+  };
 
-
-return (
-  <div className='inp'>
-    <div className='nn'>
-      <marquee behavior="" direction="">Convert your dollar to naira</marquee>
-      <input type="number" value={dollar} onChange={e => setDollar(e.target.value)} />
-      <button onClick={handleChange}>Convert</button>
-      <p>Naira : {naira}</p>
+  return (
+    <div className='inp'>
+      <div className='nn'>
+        <marquee behavior="" direction="">Convert your dollar to naira</marquee>
+        <input type="number" value={dollar} onChange={e => setDollar(e.target.value)} />
+        <button onChange={handleChange}>Convert</button>
+        <p>Naira: {naira}</p>
+      </div>
     </div>
-  </div>
-);
-}
+  );
+};
 
-
-export default Convert
+export default Convert;
